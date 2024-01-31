72231a331e54b0239b561887ddebde35c8a19334
MrMirrorr <mr.kirisenko@gmail.com>
2024-01-31 12:32:25 +0200

    first commit

---
+++ next.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/next',
+  images: {
+    unoptimized: true,
+  },
+};
+
+export default nextConfig;
