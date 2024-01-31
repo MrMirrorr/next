913952b61a654782aae3fde151a87251e8ad814a
MrMirrorr <mr.kirisenko@gmail.com>
2024-01-31 14:25:45 +0200

    Initial commit

--- rename.mjs
+++ rename.mjs
@@ -2,9 +2,9 @@ import fs from 'fs';
 import path from 'path';
 
 const oldPath = path.join(process.cwd(), 'out', 'index.txt');
-const newPath = path.join(process.cwd(), 'out', 'next.txt');
+const newPath = path.join(process.cwd(), 'next.txt');
 
 fs.rename(oldPath, newPath, (err) => {
   if (err) throw err;
-  console.log('File renamed successfully');
+  console.log('File renamed and moved successfully');
 });
