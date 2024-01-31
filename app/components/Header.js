16d87bcc51bec14adf502eea02fbfab2f8aa84ff
MrMirrorr <mr.kirisenko@gmail.com>
2024-01-31 12:54:59 +0200

    Initial commit

--- app/components/Header.js
+++ app/components/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
     <header>
       <strong>Frontend Developer</strong>
       <nav>
-        <Link href="/">Home</Link>
+        <Link href="/next">Home</Link>
         <Link href="/about">About</Link>
       </nav>
     </header>
